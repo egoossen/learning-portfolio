715ea615b7e4dff4076100884027576abe0871e7
Elijah Goossen <ekgoossen@gmail.com>
2024-03-01 23:24:07 -0600

    Adds components javascript file.

---
+++ components/components.js
@@ -0,0 +1,48 @@
+class NavMenu extends HTMLElement {
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		this.innerHTML = `
+			<header>
+				<nav>
+					<ul>
+						<li><a href="index.html">About Me</a></li>
+						<!-- <li><a href="index.html">Why KSP?</a></li>
+						<li class="dropdown">
+							<a href="javascript:void(0)" class="dropbtn">Portfolio</a>
+							<div class="dropdown-content">
+								<a href="index.html">Introduction to Rhetoric and Christian Scholarship</a>
+							</div>
+						</li>
+						<li><a href="index.html">Contact</a></li> -->
+					</ul>
+				</nav>
+			</header>
+		`;
+	}
+}
+
+customElements.define('nav-menu', NavMenu);
+
+class Footer extends HTMLElement {
+	constructor () {
+		super();
+	}
+
+	static get observedAttributes () { return ["rev-date"]; }
+	attributeChangedCallback (name, oldVal, newVal) {
+		if (name=="rev-date") { this.innerHTML = `
+			<div class="footer">
+				<address>
+					Elijah Goossen<br>
+					<a href="mailto:ekgoossen@gmail.com">ekgoossen@gmail.com</a><br>
+					Last updated: ` + newVal + `
+				</address>
+			</div>
+		`;}
+	}
+}
+
+customElements.define("my-footer", Footer);
